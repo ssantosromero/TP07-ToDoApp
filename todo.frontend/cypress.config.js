2e3b67274bc0150da7b51cdf7c4115e5dc93f707
Santos Romero <santosromeroreyna@gmail.com>
2025-11-13 18:47:02 -0300

    Fix Sonar coverage path

--- todo.frontend/cypress.config.js
+++ todo.frontend/cypress.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require("cypress");
+const codeCoverageTask = require('@cypress/code-coverage/task');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
+
     setupNodeEvents(on, config) {
-      // No usamos plugins adicionales
-    },
-  },
+      codeCoverageTask(on, config);
+      return config;
+    }
+  }
 });
