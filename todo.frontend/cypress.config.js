dd2f6b9808ec0ef7f52da01aa883541fc6d2887a
Santos Romero <santosromeroreyna@gmail.com>
2025-11-13 19:59:58 -0300

    fix: final cypress + sonarcloud config

--- todo.frontend/cypress.config.js
+++ todo.frontend/cypress.config.js
@@ -1,13 +1,12 @@
 const { defineConfig } = require("cypress");
-const codeCoverageTask = require('@cypress/code-coverage/task');
+const codeCoverageTask = require("@cypress/code-coverage/task");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-
     setupNodeEvents(on, config) {
       codeCoverageTask(on, config);
       return config;
     }
-  }
+  },
 });
