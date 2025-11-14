3c64fc92854e1cfda82638159126ce930b26073a
Santos Romero <santosromeroreyna@gmail.com>
2025-11-14 10:31:31 -0300

    test mas simple6

--- todo.frontend/cypress/e2e/smoke.cy.js
+++ todo.frontend/cypress/e2e/smoke.cy.js
@@ -1,14 +1,6 @@
 describe("Smoke test", () => {
-  it("Carga la aplicación", () => {
-    cy.visit("http://localhost:3000")
-
-    // Verifica que el título principal existe
-    cy.contains("ToDoApp – TP07").should("exist")
-
-    // Verifica que el input de Nueva tarea existe
-    cy.get('input[placeholder="Nueva tarea..."]').should("exist")
-
-    // Verifica que el botón Agregar existe
-    cy.contains("Agregar").should("exist")
-  })
-})
+  it("loads the app", () => {
+    cy.visit("http://localhost:3000");
+    cy.contains("ToDoApp – TP07").should("exist");
+  });
+});
